99610ee18e08afb198046aed3f634a6f73f72f8b
eoneee <sangeon0412@gmail.com>
2022-02-09 01:21:15 +0900

    Trigraphs

---
+++ 10926.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int main(void)
+// {
+// 	cout << "hello world" << endl;
+// 	return 0;
+// }
+
+int main()
+{
+	string id;
+	cin >> id;
+	cout << id + "?\?!\n" << endl;
+	return 0;
+}
